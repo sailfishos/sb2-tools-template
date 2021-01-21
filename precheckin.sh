ba557add0004b9b7c0b6f5f2823d1ea4707c65ba
David Greaves <david.greaves@jolla.com>
2021-01-21 12:45:26 +0000

    Remove unused architectures
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- precheckin.sh
+++ precheckin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ARCHS="armv6l armv7l armv7hl armv7thl armv7tnhl mipsel aarch64 i486"
+ARCHS="armv7hl aarch64 i486"
 
 for x in $ARCHS; do
 	sed "s/@ARCH@/$x/g" sb2-tools-template.spec > sb2-tools-$x.spec
